b927c0fa5c44738559061d158ac77c526e18fcf2
pratiksha195 <130679065+pratiksha195@users.noreply.github.com>
2024-12-02 10:49:46 +0530

    Update FirstApp.java

--- FirstApp.java
+++ FirstApp.java
@@ -3,6 +3,6 @@ public class PossibleCombinationArr
 {
 	public static void main(String x[])
 	{
-		Scanner xyz=new Scanner(System.in);
-		int size,i,j;
-		
+		System.out.println("Pratiksha Jankar");
+	}
+}
